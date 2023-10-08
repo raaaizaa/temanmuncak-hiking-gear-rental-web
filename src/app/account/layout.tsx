e75eb5858c92771b7b9a6ca2ffb70096244048f3
jasondt1 <tanuartadjason@gmail.com>
2023-10-09 01:30:14 +0700

    account page

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from "react";
+import Account from "@/components/account/accountPage";
 
 export default function AccountPage() {
-  return (
-    <div className='px-4 sm:px-12 md:px-20 lg:px-24 xl:px-32'>
-        ini account page
-    </div>
-  )
+  return <Account />;
 }
