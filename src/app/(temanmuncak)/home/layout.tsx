c8760b34a70fa4527229ffcee1f1b473e4bd16dd
jasondt1 <tanuartadjason@gmail.com>
2023-11-21 17:45:57 +0700

    review

--- src/app/(temanmuncak)/home/layout.tsx
+++ src/app/(temanmuncak)/home/layout.tsx
@@ -1,17 +1,18 @@
-'use client'
-import { AuthContext } from '@/components/context/auth-context'
-import Goals from '@/components/templates/homepage/goals'
-import Landing from '@/components/templates/homepage/landing'
-import MountainGrid from '@/components/templates/homepage/mountain-grid'
-import React, { useContext, useEffect } from 'react'
+"use client";
+import { AuthContext } from "@/components/context/auth-context";
+import Goals from "@/components/templates/homepage/goals";
+import Landing from "@/components/templates/homepage/landing";
+import MountainGrid from "@/components/templates/homepage/mountain-grid";
+import Review from "@/components/templates/homepage/review";
+import React, { useContext, useEffect } from "react";
 
 export default function HomePage() {
-  const isAuthenticated = useContext(AuthContext).isAuthenticated
+  const isAuthenticated = useContext(AuthContext).isAuthenticated;
   useEffect(() => {
     if (!isAuthenticated) {
-      window.location.replace('/login')
+      window.location.replace("/login");
     }
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -20,6 +21,7 @@ export default function HomePage() {
         <MountainGrid />
       </div>
       <Goals />
+      <Review />
     </div>
-  )
+  );
 }
